1f24e668878b01fb00c680f9db98e169ef21a904
Jorge Coria <wecodethose@gmail.com>
2022-05-20 22:20:49 -0700

    MPA in a single .py file -> Swapped in a graph of mine as one of the pages

---
+++ venv/Scratches/test_mpa_dash.py
@@ -0,0 +1,129 @@
+# Multi-Page Dash App in a single .py file.
+# Swapped in a graph of mine as one of the pages
+
+import pandas as pd
+import plotly.express as px
+from dash import Dash, html, dcc, Input, Output, State, callback
+
+
+app = Dash(__name__)
+
+df = pd.read_csv("https://raw.githubusercontent.com/Coding-with-Adam/Dash-by-Plotly/master/Other/Dash_Introduction/intro_bees.csv")
+
+df = df.groupby(['State', 'ANSI', 'Affected by', 'Year', 'state_code'])[['Pct of Colonies Impacted']].mean()
+df.reset_index(inplace=True)
+
+url_bar_and_content_div = html.Div([
+    dcc.Location(id='url', refresh=False),
+    html.Div(id='page-content')
+])
+
+layout_index = html.Div([
+    dcc.Link('Navigate to "/page-1"', href='/page-1'),
+    html.Br(),
+    dcc.Link('Navigate to "/page-2"', href='/page2'),
+])
+
+layout_page_1 = html.Div([
+    html.H2('Page 1'),
+    dcc.Input(id='input-1-state', type='text', value='Montreal'),
+    dcc.Input(id='input-2-state', type='text', value='Canada'),
+    html.Button(id='submit-button', n_clicks=0, children='Submit'),
+    html.Div(id='output-state'),
+    html.Br(),
+    dcc.Link('Navigate to "/"', href='/'),
+    html.Br(),
+    dcc.Link('Navigate to "/page-2"', href='/page2'),
+])
+
+layout_page_2 = html.Div([
+
+    html.H1("Web Application Dashboards with Dash", style={'text-align': 'center'}),
+
+    dcc.Link('Navigate to "/"', href='/'),
+    html.Br(),
+    dcc.Link('Navigate to "/page-1"', href='/page-1'),
+    html.Br(),
+    html.Br(),
+
+    dcc.Dropdown(id="slct_year",
+                 options=[
+                     {"label": "2015", "value": 2015},
+                     {"label": "2016", "value": 2016},
+                     {"label": "2017", "value": 2017},
+                     {"label": "2018", "value": 2018}],
+                 multi=False,
+                 value=2015,
+                 style={'width': "40%"}
+                 ),
+    html.Div(id='output_container', children=[]),
+    html.Br(),
+    dcc.Graph(id='my_bee_map', figure={})
+])
+
+# index layout
+app.layout = url_bar_and_content_div
+
+# "complete" layout
+app.validation_layout = html.Div([
+    url_bar_and_content_div,
+    layout_index,
+    layout_page_1,
+    layout_page_2,
+])
+
+
+# Index callbacks
+@callback(Output('page-content', 'children'),
+              Input('url', 'pathname'))
+def display_page(pathname):
+    if pathname == "/page-1":
+        return layout_page_1
+    elif pathname == "/page2":
+        return layout_page_2
+    else:
+        return layout_index
+
+
+# Page 1 callbacks
+@callback(Output('output-state', 'children'),
+              Input('submit-button', 'n_clicks'),
+              State('input-1-state', 'value'),
+              State('input-2-state', 'value'))
+def update_output(n_clicks, input1, input2):
+    return f'The Button has been pressed {n_clicks} times. \
+            Input 1 is {input1} and Input 2 is {input2}'
+
+
+# Page 2 callbacks
+@app.callback(
+    [Output(component_id='output_container', component_property='children'),
+     Output(component_id='my_bee_map', component_property='figure')],
+    [Input(component_id='slct_year', component_property='value')]
+)
+def update_graph(option_slctd):
+    print(option_slctd)
+    print(type(option_slctd))
+
+    container = "The year chosen by user was: {}".format(option_slctd)
+
+    dff = df.copy()
+    dff = dff[dff["Year"] == option_slctd]
+    dff = dff[dff["Affected by"] == "Varroa_mites"]
+
+    # Plotly Express
+    fig = px.choropleth(
+        data_frame=dff,
+        locationmode='USA-states',
+        locations='state_code',
+        scope="usa",
+        color='Pct of Colonies Impacted',
+        hover_data=['State', 'Pct of Colonies Impacted'],
+        color_continuous_scale=px.colors.sequential.YlOrRd,
+        labels={'Pct of Colonies Impacted': '% of Bee Colonies'},
+        template='plotly_dark'
+    )
+    return container, fig
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
