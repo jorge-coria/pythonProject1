76865d9ce35892991abc06f93eff7f6c9f79ce5b
Jorge Coria <wecodethose@gmail.com>
2022-05-23 22:38:11 -0700

    Added a 404 page for pathnames that are not listed in app.py

--- Dash - Multi Page App (FPS)/app.py
+++ Dash - Multi Page App (FPS)/app.py
@@ -3,6 +3,7 @@
 # This iteration of MPA does not include an index for now, so auto-404
 
 from dash import Dash, dcc, html, Input, Output, callback
+import dash_bootstrap_components as dbc
 from layouts import layout1, layout2
 import callbacks
 
@@ -23,7 +24,14 @@ def display_page(pathname):
     elif pathname == '/page2':
         return layout2
     else:
-        return '404'
+        return dbc.Container(
+            [
+                html.H1("404: Page Not Found", className="text-danger"),
+                html.Hr(),
+                html.P(f"The pathname {pathname} was not recognised..."),
+                html.Label(['If I knew the way I would take you ', dcc.Link('home', href='/')]),
+            ]
+        )
 
 
 if __name__ == '__main__':
