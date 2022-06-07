c4cdce6e251b49e5bd98b925f04d793d5f278282
Jorge Coria <wecodethose@gmail.com>
2022-06-06 21:23:57 -0700

    Match One or More with the Plus Sign

---
+++ venv/Scratches/oneOrMoreRegex.py
@@ -0,0 +1,15 @@
+# Group that precedes the (+) must appear at least once
+import re
+
+batRegex = re.compile(r'Bat(wo)+man')
+mo1 = batRegex.search('The Adventures of Batwoman')
+mo1.group()
+print('Match Object 1: ' + mo1.group())
+
+mo2 = batRegex.search('The Adventures of Batwowowowoman')
+mo2.group()
+print('Match Object 2: ' + mo2.group())
+
+mo3 = batRegex.search('The Adventures of Batman')
+mo3 == None
+print('Match Object 3: ' + mo3.group())
