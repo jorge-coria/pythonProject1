3ffba5ed861244a0c84a2658a750477ef6ebbbdb
Jorge Coria <wecodethose@gmail.com>
2022-06-07 16:30:51 -0700

    Added dot star (.*) , greedy vs non greedy

--- venv/Scratches/wildCardChar.py
+++ venv/Scratches/wildCardChar.py
@@ -6,3 +6,18 @@ import re
 atRegex = re.compile(r'.at')
 mo1 = atRegex.findall('The cat in the hat sat on the flat mat.')
 print('Match Object 1: ' + str(mo1))
+
+# The (.*) chars in a regex match any char and are greedy -> They will match as much text as possible by default
+nameRegex = re.compile(r'First Name: (.*) Last Name: (.*)')
+mo2 = nameRegex.search('First Name: Jay Last Name: Cee')
+print('Match Object 2 Group 1: ' + mo2.group(1))
+print('Match Object 2 Group 2: ' + mo2.group(2))
+
+# Use (.*?) chars to match in a non-greedy way
+nongreedyRegex = re.compile(r'<.*?>')
+mo3 = nongreedyRegex.search('<To serve man> for dinner.')
+print('Match Object 3: ' + mo3.group())
+
+greedyRegex = re.compile(r'<.*>')
+mo4 = greedyRegex.search('<To serve man> for dinner.>')
+print('Match Object 4: ' + mo4.group())
