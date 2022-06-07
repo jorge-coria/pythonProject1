f79d2ec3732e3c58656df5e4314a0b0bc44eeb93
Jorge Coria <wecodethose@gmail.com>
2022-06-07 11:58:18 -0700

    Refactor

--- venv/Scratches/greedyNonMatchRegex.py
+++ venv/Scratches/greedyNonMatchRegex.py
@@ -4,10 +4,8 @@ import re
 
 greedyHaRegex = re.compile(r'(Ha){3,5}')
 mo1 = greedyHaRegex.search('HaHaHaHaHa')
-mo1.group()
 print('Match Object 1: ' + mo1.group())
 
 nongreedyHaRegex = re.compile(r'(Ha){3,5}?')
 mo2 = nongreedyHaRegex.search('HaHaHaHaHa')
-mo1.group()
 print('Match Object 2: ' + mo2.group())
