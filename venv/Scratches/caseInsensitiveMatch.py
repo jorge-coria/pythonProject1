bba4597f994ee6c66e86910ae978de152b391234
Jorge Coria <wecodethose@gmail.com>
2022-06-08 18:57:37 -0700

    Pass re.I as second paramter to re.compile() to use case-insensitive matching in your regex

---
+++ venv/Scratches/caseInsensitiveMatch.py
@@ -0,0 +1,7 @@
+# When you want to match letters without worrying about lower/upper-case, pass re.IGNORECASE to re.compile()
+
+import re
+
+robocop = re.compile(r'robocop', re.I)
+mo1 = robocop.search('RoBoCOP is part man, part machine, all cop.')
+print('Match Object 1: ' + str(mo1.group()))
