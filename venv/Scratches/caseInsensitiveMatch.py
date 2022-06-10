efdc48b74e2129e0daf67e319455c4a8053c0c6a
Jorge Coria <wecodethose@gmail.com>
2022-06-09 23:38:22 -0700

    Passing re.I as second parameter to re.compile() --> More examples

--- venv/Scratches/caseInsensitiveMatch.py
+++ venv/Scratches/caseInsensitiveMatch.py
@@ -5,3 +5,9 @@ import re
 robocop = re.compile(r'robocop', re.I)
 mo1 = robocop.search('RoBoCOP is part man, part machine, all cop!')
 print('Match Object 1: ' + str(mo1.group()))
+
+mo2 = robocop.search('ROBOCOP is part man, part machine, all cop!')
+print('Match Object 2: ' + str(mo2.group()))
+
+mo3 = robocop.search('roboCOP is part man, part machine, all cop!')
+print('Match Object 3: ' + str(mo3.group()))
