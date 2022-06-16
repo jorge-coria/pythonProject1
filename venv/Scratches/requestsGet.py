28b96b5cecc80104314eab48dbd68fa81a4728e1
Jorge Coria <wecodethose@gmail.com>
2022-06-15 21:43:48 -0700

    Using requests module to download a webpage. Prints the first 400 characters.

--- venv/Scratches/requestsGet.py
+++ venv/Scratches/requestsGet.py
@@ -5,3 +5,14 @@ print(type(res))
 print(res.status_code == requests.codes.ok)
 print(len(res.text))
 print(res.text[:400])
+
+# Failed download not a dealbreaker for your program, wrap with try+except to handle without crashing
+res = requests.get('http://inventwithpython.com/page_that_does_not_exist')
+try:
+    res.raise_for_status()
+except Exception as exc:
+    print('There was a problem: %s' % (exc))
+
+# Check for errors, raises an exception if failed to download, does nothing if download successful
+res = requests.get('http://inventwithpython.com/page_that_does_not_exist')
+res.raise_for_status()
