8f8e5681382fe72db19d42a265bf6f8dbda855d3
Jorge Coria <wecodethose@gmail.com>
2022-06-18 07:23:15 -0700

    Scrapes and downloads all comics from XKCD website

---
+++ venv/Scratches/downloadXkcd.py
@@ -0,0 +1,42 @@
+# Downloads all XKCD Comics
+import requests, os, bs4
+
+url = 'http://xkcd.com'             # starting url
+os.makedirs('xkcd', exist_ok=True)  # store comics in ./cd
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
+    'referer': 'https://google.com',
+}
+
+while not url.endswith('#'):
+    # Download the page.
+    print('Downloading page %s...' % url)
+    res = requests.get(url, headers=headers)
+    res.raise_for_status()
+
+    soup = bs4.BeautifulSoup(res.text, features="html.parser")
+
+    # Find the URL of the comic image.
+    comicElem = soup.select('#comic img')
+    if comicElem == []:
+        print('Could not find comic image.')
+    else:
+        comicUrl = 'http:' + comicElem[0].get('src')
+    # TODO: Download the image.
+        print('Downloading image %s...' % (comicUrl))
+        print('- - - - - - - - - - - - - - - - - - - - - - - - - - - - -')
+        res = requests.get(comicUrl)
+        res.raise_for_status()
+
+    # Save the image to ./xkcd.
+        imageFile = open(os.path.join('xkcd', os.path.basename(comicUrl)), 'wb')
+        for chunk in res.iter_content(100000):
+            imageFile.write(chunk)
+        imageFile.close()
+
+    # Get the Prev button's url.
+    prevLink = soup.select('a[rel="prev"]')[0]
+    url = 'http://xkcd.com' + prevLink.get('href')
+
+print('Done.')
