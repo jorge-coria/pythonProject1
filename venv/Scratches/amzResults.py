eed21da1dbcb365677e182216dabd88ef752ab09
Jorge Coria <wecodethose@gmail.com>
2022-06-17 20:24:04 -0700

    Opens several Amazon search results from text in clipboard

--- venv/Scratches/amzResults.py
+++ venv/Scratches/amzResults.py
@@ -5,7 +5,7 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
     'referer': 'https://google.com',
 }
-print('Checking Amazon...')        # display text while downloading the Google page
+print('Checking Amazon...')        # display text while downloading the Amazon page
 res = requests.get('https://www.amazon.com/s?k=' + ''.join(pyperclip.paste()), headers=headers)
 print('\'' + str(pyperclip.paste()) + '\'')
 res.raise_for_status()
