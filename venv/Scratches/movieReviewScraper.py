aa92579e50f99ece0ed422ba160346ae2a27b302
Jorge Coria <wecodethose@gmail.com>
2022-07-01 22:16:21 -0700

    Used Selenium + BS4 to scrape and print Rotten Tomatoes Movie text reviews using exact search term from clipboard

--- venv/Scratches/movieReviewScraper.py
+++ venv/Scratches/movieReviewScraper.py
@@ -1,36 +1,54 @@
+# Scrapes the Rotten Tomatoes text reviews of a movie title read from user's clipboard
+
+import bs4, pyperclip, time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-import pyperclip
 
-
+# Open an incognito Chrome tab that stays open
+o = webdriver.ChromeOptions()
+o.add_argument('--incognito')
+o.add_experimental_option('detach', True)
 s = Service('C:/Users/Home/chromedriver.exe')
-browser = webdriver.Chrome(service=s)
+browser = webdriver.Chrome(service=s,options=o)
 browser.get('https://www.rottentomatoes.com/')
 
-searchInputElem = WebDriverWait(browser, 10).until(
+# Set searchTerm using clipboard content and use it as the search input
+searchTerm = pyperclip.paste()
+searchInputElem = WebDriverWait(browser, 13).until(
     EC.presence_of_element_located((By.TAG_NAME, 'input'))
 )
-searchInputElem.send_keys(pyperclip.paste())
+searchInputElem.send_keys(searchTerm)
 searchInputElem.send_keys(u'\ue007')
 
-WebDriverWait(browser, 10).until(
+# movieListElems is a list of movie titles returned as search results
+WebDriverWait(browser, 20).until(
     EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'a[slot]'))
 )
 movieListElems = browser.find_elements(By.CSS_SELECTOR, 'a[slot]')
+
+# Iterate through search results for an exact match to our searchTerm and click it
 for i in range(len(movieListElems)):
-    print(movieListElems[i].text)
+    if searchTerm == movieListElems[i].text:
+        movieListElems[i].click()
+        break
 
-# TODO: Compare the list of text to search term, click the link of closest match
+# reviewLink is a link to a full page of audience reviews, click it
+WebDriverWait(browser, 20).until(
+    EC.presence_of_element_located((By.CLASS_NAME, 'mop-audience-reviews__view-all--link'))
+)
+reviewLink = browser.find_element(By.CSS_SELECTOR, 'a[data-qa=audience-reviews-view-all-link]')
+reviewLink.click()
 
-# TODO: Scrape Reviews from Movie WebPage
+time.sleep(5)
 
+# Create a soup object and select the Review text on the current page
+html = browser.page_source
+soup = bs4.BeautifulSoup(html, features="html.parser")
+reviewText = soup.select('p[data-qa=review-text]')
 
-# buttonElem = browser.find_element(By.CSS_SELECTOR, 'button')
-# buttonElem.click()
-
-# passwordElem = browser.find_element_by_id('Passwd')
-# passwordElem.send_keys('12345')
-# passwordElem.submit()
+# Print out the list of audience reviews on the current page
+for i in range(len(reviewText)):
+    print('Review #' + str(i+1) + ': ' + str(reviewText[i].text))
