697dfc4bb3b388105fa6f8962f3fd46ac0ff075f
Jorge Coria <wecodethose@gmail.com>
2022-07-05 22:50:00 -0700

    Removed redundant code. Adjusting to python naming conventions. Lowered timeout time on WebDriverWait functions.

--- venv/Scratches/movieScraperv2.py
+++ venv/Scratches/movieScraperv2.py
@@ -7,53 +7,53 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
+
+# Create a soup object of current page and print out a list of the audience reviews
 def get_page_review_soup():
-    # Create a soup object and select the Review text on the current page
     html = browser.page_source
     soup = bs4.BeautifulSoup(html, features="html.parser")
-    reviewText = soup.select('p[data-qa=review-text]')
+    page_review_text = soup.select('p[data-qa=review-text]')
 
-    # Print out the list of audience reviews on the current page
-    for i in range(len(reviewText)):
-        print('Review #' + str(i + 1) + ': ' + str(reviewText[i].text))
+    for i in range(len(page_review_text)):
+        print('Review #' + str(i + 1) + ': ' + str(page_review_text[i].text))
 
+
+# Clicks the next-page link to navigate to the next page of movie reviews
 def click_next_page_link():
-    # nextPageLink takes us to next page of movie reviews, click it
-    WebDriverWait(browser, 20).until(
-        EC.presence_of_element_located((By.CLASS_NAME, 'prev-next-paging__button-text'))
+    next_page_link = WebDriverWait(browser, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, 'button[data-qa=next-btn]'))
     )
-    nextPageLink = browser.find_element(By.CSS_SELECTOR, 'button[data-qa=next-btn]')
-    nextPageLink.click()
+    next_page_link.click()
 
     time.sleep(5)
+
 
 # Open an incognito Chrome tab that stays open
 o = webdriver.ChromeOptions()
 o.add_argument('--incognito')
 o.add_experimental_option('detach', True)
 s = Service('C:/Users/Home/chromedriver.exe')
-browser = webdriver.Chrome(service=s,options=o)
+browser = webdriver.Chrome(service=s, options=o)
 searchTerm = pyperclip.paste()
 browser.get('https://www.rottentomatoes.com/search?search=' + ''.join(searchTerm))
 
-# movieListElems is a list of movie titles returned as search results
-WebDriverWait(browser, 20).until(
+# List of movie titles returned as search results
+movie_title_list = WebDriverWait(browser, 10).until(
     EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'a[slot]'))
 )
-movieListElems = browser.find_elements(By.CSS_SELECTOR, 'a[slot]')
 
 # Iterate through search results for an exact match to our searchTerm and click it
-for i in range(len(movieListElems)):
-    if searchTerm == movieListElems[i].text:
-        movieListElems[i].click()
+for i in range(len(movie_title_list)):
+    if searchTerm == movie_title_list[i].text:
+        movie_title_list[i].click()
         break
 
-# reviewLink is a link to a full page of audience reviews, click it
-WebDriverWait(browser, 20).until(
-    EC.presence_of_element_located((By.CLASS_NAME, 'mop-audience-reviews__view-all--link'))
+# Click link to navigate to the full page of audience reviews
+reviewLink = WebDriverWait(browser, 10).until(
+    EC.presence_of_element_located((By.CSS_SELECTOR, 'a[data-qa=audience-reviews-view-all-link]'))
 )
-reviewLink = browser.find_element(By.CSS_SELECTOR, 'a[data-qa=audience-reviews-view-all-link]')
 reviewLink.click()
+
 time.sleep(5)
 
 get_page_review_soup()
