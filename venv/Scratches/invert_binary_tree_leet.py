df3dda5bc5ceb8ff45a9c4c8b20e106471f31753
Jorge Coria <wecodethose@gmail.com>
2022-07-13 23:29:22 -0700

    Solving Leetcode: Invert Binary Tree - Python. Time: O(n) | Memory: O(h)

---
+++ venv/Scratches/invert_binary_tree_leet.py
@@ -0,0 +1,25 @@
+# Recursively inverting binary tree using tmp node and swapping left and right nodes
+# Time Complexity: O(n) | Memory: O(h) -> h is the height of the tree
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def invertTree(root: TreeNode):
+        if not root:
+            return False
+
+        tmp = root.left
+        root.left = root.right
+        root.right = tmp
+
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+
+        return root
+
+q = [2, 1, 3]
+print(Solution.invertTree(q))
