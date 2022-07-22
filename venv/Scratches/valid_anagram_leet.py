1bde023b6c9de48dbdc8f895bba37ae3f271b935
Jorge Coria <wecodethose@gmail.com>
2022-07-21 23:27:37 -0700

    Solving Leetcode: Valid Anagram - Python. Time: O(s + t) | Memory: O(s + t)

--- venv/Scratches/valid_anagram_leet.py
+++ venv/Scratches/valid_anagram_leet.py
@@ -22,8 +22,10 @@ a, b = 'anagram', 'nagaram'
 c, d = 'rat', 'car'
 e, f = 'brag', 'grab'
 r, s = 'disney', 'neydis'
+t, u = 'neydis', 'disney'
 
 print('Is '+ a + ' an anagram of ' + b + '? >> ' + str(Solution.isAnagram(a, b)))
 print('Is '+ c + ' an anagram of ' + d + '? >> ' + str(Solution.isAnagram(c, d)))
 print('Is '+ e + ' an anagram of ' + f + '? >> ' + str(Solution.isAnagram(e, f)))
 print('Is '+ r + ' an anagram of ' + s + '? >> ' + str(Solution.isAnagram(r, s)))
+print('Is '+ t + ' an anagram of ' + u + '? >> ' + str(Solution.isAnagram(t, u)))
